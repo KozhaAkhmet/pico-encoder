1eadf57aaeb69ef0d4276f375c9d9020d6766020
KozhaAkhmet <kozha24082001@gmail.com>
2023-09-27 21:05:48 +0300

    Removed Unnecessary

--- encoder/Encoder.cpp
+++ encoder/Encoder.cpp
@@ -32,38 +32,3 @@ void Encoder::setDir(int dir){
 int Encoder::getCounter() {
     return this->counter;
 }
-
-
-// uint out_pin = 21;
-
-// const int number_of_holes = 20;
-// bool state = false;
-// int hole_count = 0;
-// int dir = 1;
-
-// void count(){
-//     hole_count =  hole_count >= number_of_holes ?
-//                             (hole_count % number_of_holes) + dir :
-//                             hole_count + dir;
-// }
-
-// void setup(){
-//     stdio_init_all();
-//     gpio_init(out_pin);
-//     gpio_set_dir(out_pin, GPIO_IN);
-// }
-
-// int main(){
-//     setup();
-//     while(true){
-//         if(gpio_get(out_pin) == 0 ){
-//             state = true;
-//         }else
-//         if(gpio_get(out_pin) == 1 && state == true){
-//             count();
-//             state = false;
-//         }
-//         printf("\n %d", hole_count);
-//         sleep_ms(10);
-//     }
-// }
