c1ba24e5f1694587ea18ebd25cee37717be168af
KozhaAkhmet <kozha24082001@gmail.com>
2023-09-28 12:03:40 +0300

    Added Comments

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -9,7 +9,8 @@ void Encoder::Setup() {
     pinMode(this->out_pin, INPUT);
 }
 
-// This function begins to count LOW to HIGH switches. Use this function in loop.
+// This function begins to count LOW to HIGH switches. Also, function will take reminder from count value by total_amount, so the count value will be resetted after reaching the total_amount. This includes negative values.
+// Use this function in loop.
 void Encoder::Count() {
     if (digitalRead(this->out_pin) == 0) {
         state = true;
@@ -22,10 +23,12 @@ void Encoder::Count() {
     }
 }
 
+//You can set the direction, so the count value will be added or subtracted by given parameter. For example, passing 1 or -1.
 void Encoder::setDir(int dir){
     this->dir = dir;
 }
 
+//Get the counter value.
 int Encoder::getCounter() {
     return this->counter;
 }
