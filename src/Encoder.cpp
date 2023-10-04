d2a4cb4f16e88753908c31d2e839938e37f99296
KozhaAkhmet <kozha24082001@gmail.com>
2023-10-04 17:04:51 +0300

    Added per millisecond count

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -1,34 +1,59 @@
 #include "Encoder.h"
 
-Encoder::Encoder(int out_pin, int total_amount) {
+Encoder::Encoder(int out_pin, int total_steps) {
     this->out_pin = out_pin;
-    this->total_amount = total_amount;
+    this->total_steps = total_steps;
 }
-
 void Encoder::Setup() {
     pinMode(this->out_pin, INPUT);
 }
 
-// This function begins to count LOW to HIGH switches. Also, function will take reminder from count value by total_amount, so the count value will be resetted after reaching the total_amount. This includes negative values.
+// This function begins to count LOW to HIGH switches. Do not forget to call reset() or buffer overflow will happen.
 // Use this function in loop.
-void Encoder::Count() {
+void Encoder::lapCount() {
     if (digitalRead(this->out_pin) == 0) {
         state = true;
-    } else if (digitalRead(this->out_pin == 1 && state == true) {
-        this->counter = this->counter >= this->total_amount ||
-                            this->counter <= -this->total_amount ?
-                            (this->counter % this->total_amount) + this->dir :
-                            this->counter + this->dir;
+    } else if (digitalRead(this->out_pin == 1 && state == true)) {
+        this->steps_c = this->steps_c + this->dir;
         state = false;
     }
 }
 
-//You can set the direction, so the count value will be added or subtracted by given parameter. For example, passing 1 or -1.
-void Encoder::setDir(int dir){
+// This function count steps in milliseconds. Per second by default.
+// Use this in loop.
+void Encoder::countPerMilliSecond(int32_t time_amount) {
+    now = millis();
+
+    // printf(" time is :%d \n", to_ms_since_boot(get_absolute_time()));
+
+    if (0 > (now - stop)) {
+        // Counting the steps.
+        lapCount();
+    } else {
+        // Updating the time step and assigning the steps_per_sec. Resetting the steps value.
+        stop = now + time_amount;
+        this->steps_per_sec = getSteps();
+        reset();
+    }
+}
+
+// You can set the direction, so the count value will be added or subtracted by given parameter, like passing 1 or -1.
+void Encoder::setDir(int dir) {
     this->dir = dir;
 }
 
-//Get the counter value.
-int Encoder::getCounter() {
-    return this->counter;
+// Get the steps value.
+int32_t Encoder::getSteps() {
+    return this->steps_c;
 }
+
+// Get the value from Encoder::countPerSeconds.
+int32_t Encoder::getStepsPerMilliSecond() {
+    return this->steps_per_sec;
+}
+
+// Resetting the steps to 0.
+void Encoder::reset() {
+    this->steps_c = 0;
+}
+
--- src/Encoder.h
+++ src/Encoder.h
@@ -4,18 +4,24 @@
 
 class Encoder{
     private:
-    int counter = 0;
+    int steps_c = 0; //step_c
+    int steps_per_sec = 0; // step_per_sec
     int dir = 0;
     bool state = false;
     int out_pin;
-    int total_amount;
+    int total_steps; // total_steps
+    int32_t now = 0;
+    int32_t stop = 0;
 
     public:
     Encoder(int out_pin, int total_amount);
     void Setup();
-    void Count();
-    int getCounter();
+    void lapCount();
+    int32_t getSteps();
     void setDir(int dir);
+    void reset();
+    void countPerMilliSecond(int32_t time_amount = 1000);
+    int32_t getStepsPerMilliSecond();
 };
 
 #endif
